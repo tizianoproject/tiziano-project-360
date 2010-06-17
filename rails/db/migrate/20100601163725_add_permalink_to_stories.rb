db3321f1f46e0286bf4c320d2f3fdb9ae8defb42
cmendez@tizianoproject.org <cmendez@tizianoproject.org@c4efbacc-5089-cc8e-602a-03591289dc70>
2010-06-17 11:02:17 +0000

    Removing Rails App.  Possibly starting a new one using SQLite3 DB

--- rails/db/migrate/20100601163725_add_permalink_to_stories.rb
+++
@@ -1,9 +0,0 @@
-class AddPermalinkToStories < ActiveRecord::Migration
-  def self.up
-    add_column :stories, :permalink, :string
-  end
-
-  def self.down
-    remove_column :stories, :permalink
-  end
-end
